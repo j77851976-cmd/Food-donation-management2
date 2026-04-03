8826b23d6100f01bd52e9d17485fe65e25b2b8cd
Donald jefferson <donald@Mac.lan>
2026-04-03 11:36:12 +0530

    fix: replace Express 5 incompatible wildcard * with regex /.*/

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ const __dirname = path.dirname(__filename);
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
 
-  app.get('*', (req, res) =>
+  app.get(/.*/, (req, res) =>
     res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'))
   );
 } else {
